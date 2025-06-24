569a7230fc6cd241e773159082e9c4df999c0970
Shaikhzai <shaikhston1123@gmail.com>
2025-06-24 19:23:12 +0530

    NavBar responsive fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,35 @@
-import React from 'react'
-import '../styles/navbar.css'
+import React from 'react';
+import '../styles/navbar.css';
 
 const Navbar = () => {
   return (
-<div className="nav-bar">
+    <header className="nav-bar">
       <img
         className="logo-2"
         alt="Logo"
         src="https://c.animaapp.com/mc8ufun6NECYwc/img/logo-2.svg"
       />
 
-      <div className="menu">
-        <div className="link-6">
-          <div className="link-7">Platform</div>
-        </div>
-
-        <div className="link-6">
-          <div className="link-7">Solutions</div>
-        </div>
-
-        <div className="link-6">
-          <div className="link-7">Developers</div>
-        </div>
-
-        <div className="link-6">
-          <div className="link-7">Resources</div>
-        </div>
-
-        <div className="link-6">
-          <div className="link-7">Pricing</div>
-        </div>
-      </div>
+      <nav className="menu">
+        {['Platform', 'Solutions', 'Developers', 'Resources', 'Pricing'].map((item) => (
+          <div className="link-6" key={item}>
+            <div className="link-7">{item}</div>
+          </div>
+        ))}
+      </nav>
 
       <div className="actions">
         <div className="link-8">
           <div className="link-9">Log in</div>
         </div>
 
-        <button className="button-2">
+        <button className="button-2 gradient-button">
           <div className="highlight-2" />
-
           <div className="text-3">Schedule a demo</div>
         </button>
       </div>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
